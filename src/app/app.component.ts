093dadd922fe6697e266c51b08302f4fff86d989
shourov-sorkar <18103142@iubat.edu>
2022-10-06 12:04:58 +0600

    :tada: Add Bookmark Modal design complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'bookmark-manager';
+export class AppComponent implements OnInit {
+  AddBookmarkForm!: FormGroup;
+
+  ngOnInit(): void {}
 }
