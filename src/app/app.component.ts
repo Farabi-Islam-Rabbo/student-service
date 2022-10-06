1f1bd5c007f5a4784614700c2a85a3d69940980b
shourov-sorkar <18103142@iubat.edu>
2022-10-06 14:04:22 +0600

    :sparkles: Set and get bookmark data complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,15 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent implements OnInit {
   AddBookmarkForm!: FormGroup;
+  categoryData: string[] = [];
+  dataList: any[] = [];
+  disableInput: boolean = true;
+  bookMarkData: any[] = [];
 
   ngOnInit(): void {
     this.initializeFrom();
+    this.getCategory();
+    this.getBookmarkData();
   }
   initializeFrom() {
     this.AddBookmarkForm = new FormGroup({
@@ -22,17 +28,60 @@ export class AppComponent implements OnInit {
           '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
         ),
       ]),
-      category: new FormControl('', [Validators.required]),
+      category: new FormControl('0', [Validators.required]),
+      categoryinput: new FormControl(''),
     });
   }
 
   saveData() {
-    let formData = this.AddBookmarkForm.value;
-    window.localStorage.setItem('gridGrid', JSON.stringify(formData));
+    // form Data save
+    var formValue = [];
+    let retrievedData = localStorage.getItem('Grid');
+    console.log('getItem', JSON.stringify(retrievedData));
+
+    if (retrievedData) {
+      let saveData = JSON.parse(retrievedData ?? '');
+      formValue.push(saveData);
+      let formData = this.AddBookmarkForm.value;
+      formValue.push(formData);
+      localStorage.setItem('Grid', JSON.stringify(formValue));
+      alert('Bookmark Saved!');
+      this.ngOnInit();
+    } else {
+      localStorage.setItem('Grid', JSON.stringify(this.AddBookmarkForm.value));
+      alert('Bookmark Saved!');
+      this.ngOnInit();
+    }
+
+    // Category data save
+    if (!this.disableInput) {
+      let category = this.AddBookmarkForm.value.categoryinput;
+      this.categoryData.push(category);
+      localStorage.setItem('category', JSON.stringify(this.categoryData));
+    } else {
+      let category = this.AddBookmarkForm.value.category;
+      this.categoryData.push(category);
+      localStorage.setItem('category', JSON.stringify(this.categoryData));
+    }
   }
 
   addCategory() {
-    let category = this.AddBookmarkForm.value.category;
-    localStorage.setItem('category', JSON.stringify(category));
+    this.disableInput = false;
+    this.AddBookmarkForm.value.category =
+      this.AddBookmarkForm.value.categoryinput;
+  }
+
+  getCategory() {
+    let retrievedData = localStorage.getItem('category');
+    let categories = JSON.parse(retrievedData ?? '');
+    this.dataList = categories;
+    console.log('categories', categories);
+  }
+
+  getBookmarkData() {
+    let retrievedData = localStorage.getItem('Grid');
+    let bookMarkData = JSON.parse(retrievedData ?? '');
+    this.bookMarkData = bookMarkData;
+    console.log('Bookmark data', bookMarkData);
   }
 }
