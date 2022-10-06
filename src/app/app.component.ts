bb59ee2cfd79f795ce61ea548401ef5b6b626b2a
shourov-sorkar <18103142@iubat.edu>
2022-10-06 12:41:23 +0600

    :tada: Validation Added in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -10,5 +10,29 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent implements OnInit {
   AddBookmarkForm!: FormGroup;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeFrom();
+  }
+  initializeFrom() {
+    this.AddBookmarkForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      url: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+        ),
+      ]),
+      category: new FormControl('', [Validators.required]),
+    });
+  }
+
+  saveData() {
+    let formData = this.AddBookmarkForm.value;
+    window.localStorage.setItem('gridGrid', JSON.stringify(formData));
+  }
+
+  addCategory() {
+    let category = this.AddBookmarkForm.value.category;
+    localStorage.setItem('category', JSON.stringify(category));
+  }
 }
